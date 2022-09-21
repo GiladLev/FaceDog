43dae141917f8cade75079838475d24e3edaa8dd
GiladLev <giladlev2000@gmail.com>
2022-09-21 15:43:05 +0300

    client homepage work

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+
+import { Box, Stack } from "@mui/system";
+import React from "react";
+import "./app.css";
+import { Add } from "./components/Add";
+import Feed from "./components/Feed";
+import Navbar from "./components/Navbar";
+import Rightbar from "./components/Rightbar";
+import Sidebar from "./components/Sidebar";
+const App = () => {
+  return (
+    <Box>
+      <Navbar/>
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+      <Add/>
+    </Box>
+  );
+};
+export default App;
